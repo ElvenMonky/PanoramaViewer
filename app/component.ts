16eddaaf06d42e7ee958826bcc238ccb58e8b64e
Serhii Hrynko <serhii.hrynko@akvelon.com>
2017-09-11 16:54:21 +0300

    Initial client side template

---
+++ app/component.ts
@@ -0,0 +1,21 @@
+﻿import { Component } from '@angular/core';
+
+export class Panorama {
+    constructor(public src: string) {}
+}
+
+@Component({
+    selector: 'panorama-viewer',
+    template: `
+    <ul>
+        <li *ngFor="let item of items">
+            <img src="{{item.src}}" />
+        </li>
+    </ul>`
+})
+export class AppComponent {
+    items: Panorama[] =
+    [
+        { src: "panorama1.png" }
+    ];
+}
---
+++ app/module.ts
@@ -0,0 +1,10 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
