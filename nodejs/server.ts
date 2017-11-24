a929e440f52ede451dc0157937ca8b215f9926b2
Serhii Hrynko <serhii.hrynko@akvelon.com>
2017-11-24 12:29:50 +0200

    prepared for alternative server and client implementations

--- nodejs/server.ts
+++ nodejs/server.ts
@@ -7,12 +7,11 @@ var app = createExpressServer({
     routePrefix: "/api",
     controllers: [__dirname + "/controllers/*.js"]
 });
-app.use(express.static(__dirname + '/../openapi'));
-app.use(express.static(__dirname + '/../../angular/bin'));
+app.use(express.static(__dirname + '/openapi'));
+app.use(express.static(__dirname + '/client'));
 app.use('/images', express.static(__dirname + '/images'));
 
 const port: number = process.env.PORT || 1337;
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/`);
 });
-
